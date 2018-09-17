f8a55969a56820671c567d1ccd914f620425f1a9
Liz <lizlie.holleza@awsys-i.com>
2018-09-17 14:30:31 +0800

    create the editor activity and add variables

---
+++ app/src/main/java/com/example/lizlieholleza/inventoryapp/EditorActivity.java
@@ -0,0 +1,36 @@
+package com.example.lizlieholleza.inventoryapp;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private static final int EXISTING_INV_LOADER = 0;
+    private Uri currentInvUri;
+    private EditText nameEditText;
+    private EditText priceEditText;
+    private EditText quantityEditText;
+    private boolean invHasChanged = false;
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+}
