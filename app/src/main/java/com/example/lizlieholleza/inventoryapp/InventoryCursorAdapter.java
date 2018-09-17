79decb7a4e1597d83ee47bfd965b33a57758c2a4
Liz <lizlie.holleza@awsys-i.com>
2018-09-17 11:38:53 +0800

    update the bindView method

--- app/src/main/java/com/example/lizlieholleza/inventoryapp/InventoryCursorAdapter.java
+++ app/src/main/java/com/example/lizlieholleza/inventoryapp/InventoryCursorAdapter.java
@@ -6,6 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.lizlieholleza.inventoryapp.data.InventoryContract.InventoryEntry;
+import com.example.lizlieholleza.inventoryapp.data.InventoryContract;
+
+import org.w3c.dom.Text;
 
 public class InventoryCursorAdapter extends CursorAdapter {
 
@@ -20,6 +26,25 @@ public class InventoryCursorAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        TextView nameTextView = (TextView) findViewById(R.id.name);
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity);
+        TextView priceTextView = (TextView) findViewById(R.id.price);
+
+        int imageIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INV_PICTURE);
+        int nameIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INV_NAME);
+        int quantityIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INV_QTY_AVAILABLE);
+        int priceIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_INV_PRICE);
+
+        int image = cursor.getInt(imageIndex);
+        String name = cursor.getString(nameIndex);
+        int quantity = cursor.getInt(quantityIndex);
+        int price = cursor.getInt(priceIndex);
+
+        imageView.setImageResource(image);
+        nameTextView.setText(name);
+        quantityTextView.setText(quantity);
+        priceTextView.setText(price);
 
     }
 }
