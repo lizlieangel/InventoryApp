0adcabd7da9b980345412a97b523e20b4ab159ac
Liz <lizlie.holleza@awsys-i.com>
2018-09-17 10:41:21 +0800

    add constructor and update the newView method

---
+++ app/src/main/java/com/example/lizlieholleza/inventoryapp/InventoryCursorAdapter.java
@@ -0,0 +1,25 @@
+package com.example.lizlieholleza.inventoryapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+public class InventoryCursorAdapter extends CursorAdapter {
+
+    public InventoryCursorAdapter(Context context, Cursor cursor) {
+        super(context, cursor, 0);
+    }
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Inflate a list item view using the layout specified in the list_item
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
