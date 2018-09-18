a13e7fc0ff68453b3a77c202f56e100318286fff
Liz <lizlie.holleza@awsys-i.com>
2018-09-18 11:31:16 +0800

    update the oncreate method

--- app/src/main/java/com/example/lizlieholleza/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/lizlieholleza/inventoryapp/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.lizlieholleza.inventoryapp;
 
+import android.content.ContentUris;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import com.example.lizlieholleza.inventoryapp.data.InventoryContract.InventoryEntry;
+import com.example.lizlieholleza.inventoryapp.data.InventoryContract;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int INV_LOADER = 0;
+
+    InventoryCursorAdapter cursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ListView invListView = (ListView) findViewById(R.id.list_item);
+        View emptyView = findViewById(R.id.empty_view);
+        invListView.setEmptyView(emptyView);
+        cursorAdapter = new InventoryCursorAdapter(this, null);
+        invListView.setAdapter(cursorAdapter);
+        invListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                Uri currentPetUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id);
+                intent.setData(currentPetUri);
+                startActivity(intent);
+            }
+        });
+        getLoaderManager().initLoader(INV_LOADER, null, this);
     }
 }
